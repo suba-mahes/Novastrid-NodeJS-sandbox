213a4f00e5af862a3131522c526270053a5f6185
suba-mahes <insu041831@gmail.com>
2024-03-19 17:05:04 +0530

    error correction

--- sequlize-association/myapp-curd/routes/general.js
+++ sequlize-association/myapp-curd/routes/general.js
@@ -5,8 +5,8 @@ var router = express.Router();
 
 router.get('/get-only-allactors',general.findAllActor);
 router.get('/get-only-actor-by-id/:id',general.findIDActor);
-router.get('/get-only-allmovies',general.findAll);
-router.get('/get-only-movie-by-id/:id',general.findID);
+router.get('/get-only-allmovies',general.findAllMovie);
+router.get('/get-only-movie-by-id/:id',general.findIDMovie);
 
 router.post('/insert',general.create);
 router.put('/update/:id',general.update);
