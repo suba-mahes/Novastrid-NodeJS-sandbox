69123eef8ac575c223b0191ba94ace666bc76174
suba-mahes <insu041831@gmail.com>
2024-03-26 15:07:06 +0530

    changes

--- curd/myapp/validation/joi/validation.js
+++ curd/myapp/validation/joi/validation.js
@@ -4,13 +4,13 @@ var actor_movie_schema = require("./actor_movie_schema");
 
 
 module.exports.validation_actor = (data)=>{
-    return actor_schema.create_actor_schema.validate(data);
+    return actor_schema.create_actor_schema.validate(data, { abortEarly: false });
 }
 
 module.exports.validation_movie = (data)=>{
-    return movie_schema.create_movie_schema.validate(data);
+    return movie_schema.create_movie_schema.validate(data, { abortEarly: false });
 }
 
 module.exports.validation_actor_movie = (data)=>{
-    return actor_movie_schema.actor_movie_schema.validate(data);
+    return actor_movie_schema.actor_movie_schema.validate(data, { abortEarly: false });
 }
