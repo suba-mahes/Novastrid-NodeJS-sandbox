9c404dd0b9b6a78b01bf3eec5ee792808ebb64d4
suba-mahes <insu041831@gmail.com>
2024-03-26 14:49:01 +0530

    added validation error messages

--- curd/myapp/validation/joi/user_schema.js
+++ curd/myapp/validation/joi/user_schema.js
@@ -5,11 +5,15 @@ exports.user_data_schema = Joi.object({
     first_name: Joi.string().alphanum().min(3).max(30).required().messages({
       "any.required" : "first_name is required",
       "string.empty" : "first name cannot be empty",
+      "string.min" : "first name should have atleast 3 minimum characters",
+      "string.max" : "first name should have only 3o maximum characters",
       "string.alphanum" : "first name should be alpha numeric",
     }),
     last_name: Joi.string().alphanum().min(3).max(30).required().messages({
       "any.required" : "last_name is required",
       "string.empty" : "last name cannot be empty",
+      "string.min" : "last name should have atleast 3 minimum characters",
+      "string.max" : "last name should have only 3o maximum characters",
       "string.alphanum" : "last name should be alpha numeric",
     }),
     email_id: Joi.string().email().required().messages({
