499e2f6e693a11cfdcf282edef58674a19fb139b
suba-mahes <insu041831@gmail.com>
2024-04-22 15:59:29 +0530

    added changes

--- APIs/myapp/controller/html_to_pdf/html_to_pdf.js
+++ APIs/myapp/controller/html_to_pdf/html_to_pdf.js
@@ -12,32 +12,34 @@ const uploadDir = "/node_js/Novastrid-NodeJS-sandbox/APIs/myapp/pdf-download";
 exports.using_puppeteer = async (req, res) => {
   try {
     const html = req.body;
-    console.log(html);
+
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
     await page.setContent(html);
     const pdf_buffer = await page.pdf({ format: "A4" });
-    console.log(pdf_buffer);
+
     const filename = `output_pdf_puppeteer_${Date.now()}.pdf`;
     const file_path = path.join(uploadDir, filename);
     try {
-      fs.writeFile(file_path, pdf_buffer, (err) => { // Provide a callback function here
-        if (err) {
-          display.end_result(res, err.status || 500, {
-            message: err.message || "Some error occurred.",
-          });
-        } else {
-          display.end_result(res, 200, { message: "File is uploaded successfully" });
-        }
+      fs.writeFileSync(file_path, pdf_buffer);
+      res.set({
+        "Content-Type": "application/pdf",
+        "Content-Disposition": "attachment; filename=" + filename,
       });
-      //    res.sendFile(file_path);
-
-      //   res.set({
-      //     'Content-Type': 'application/pdf',
-      //     "Content-Disposition": 'attachment; filename='+ filename,
+      res.send(pdf_buffer);
+      //   (err) => {
+      //   // Provide a callback function here
+      //   if (err) {
+      //     display.end_result(res, err.status || 500, {
+      //       message: err.message || "Some error occurred.",
+      //     });
+      //   } else {
+      //     display.end_result(res, 200, {
+      //       message: "File is uploaded successfully",
+      //     });
+      //   }
       // });
-      // res.send(pdf_buffer);
-
+      //    res.sendFile(file_path);
     } catch (error) {
       display.end_result(res, error.status || 500, {
         message: error.message || "Some error occurred.",
@@ -64,27 +66,31 @@ exports.using_html_pdf = async (req, res) => {
         const filename = `output_pdf_html-pdf_${Date.now()}.pdf`;
         const file_path = path.join(uploadDir, filename);
         try {
-          fs.writeFile(file_path, buffer, (err) => { // Provide a callback function here
-            if (err) {
-              display.end_result(res, err.status || 500, {
-                message: err.message || "Some error occurred.",
-              });
-            } else {
-              display.end_result(res, 200, { message: "File is uploaded successfully" });
-            }
+          fs.writeFileSync(file_path, buffer);
+          res.set({
+            "Content-Type": "application/pdf",
+            "Content-Disposition": "attachment; filename=" + filename,
           });
+          res.send(buffer);
+
+          // , (err) => {
+          //   // Provide a callback function here
+          //   if (err) {
+          //     display.end_result(res, err.status || 500, {
+          //       message: err.message || "Some error occurred.",
+          //     });
+          //   } else {
+          //     display.end_result(res, 200, {
+          //       message: "File is uploaded successfully",
+          //     });
+          //   }
+          // });
           //    res.sendFile(file_path);
         } catch (error) {
           display.end_result(res, error.status || 500, {
             message: error.message || "Some error occurred.",
           });
         }
-
-        // res.set({
-        //     'Content-Type': 'application/pdf',
-        //     "Content-Disposition": 'attachment; filename='+ filename,
-        // });
-        // res.send(buffer);
       }
     });
   } catch (err) {
@@ -97,12 +103,12 @@ exports.using_html_pdf = async (req, res) => {
 exports.using_pdfkit = async (req, res) => {
   try {
     const html = req.body;
-
     const filename = `output_pdf_pdfkit_${Date.now()}.pdf`;
-    const file_path = path.join(uploadDir, filename);
+    const file_path = path.join(__dirname, filename);
 
     const doc = new pdf_document();
     const buffers = [];
+
     doc.pipe(
       new (require("stream").Writable)({
         write(chunk, encoding, callback) {
@@ -112,9 +118,10 @@ exports.using_pdfkit = async (req, res) => {
         final(callback) {
           res.set({
             "Content-Type": "application/pdf",
-            "Content-Disposition": "attachment; filename=" + filename,
+            "Content-Disposition": `attachment; filename=${filename}`,
           });
-          res.end(Buffer.concat(buffers));
+          res.send(Buffer.concat(buffers));
+          fs.writeFileSync(file_path, Buffer.concat(buffers));
           callback();
         },
       })
